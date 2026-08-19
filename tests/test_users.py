933b53452c16b86d8c4b450456f8316a2a5fdfb4
amos <amoschikodiri1@gmail.com>
2026-08-19 08:33:42 +0100

    Refactor: Move database fixtures to tests/database.py for reusability

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,61 +1,5 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.main import app
 from app import schemas
-from app.config import settings
-from app.database import get_db
-from app.database import Base
-
-DATABASE_URL = f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
-engine = create_engine(DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base.metadata.create_all(bind=engine)
-
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-    
-        
-app.dependency_overrides[get_db] = override_get_db
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-    
-
+from .database import client, session
 
 def test_root(client):
     res = client.get("/")
@@ -65,5 +9,10 @@ def test_root(client):
     
 def test_create_user(client):
     res = client.post("/users/", json={"email": "test@example.com", "password": "password123"})
-    print(res.json())
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "test@example.com"
     assert res.status_code == 201
+
+def test_login_user(client):
+    res = client.post("/login", data={"username": "test@example.com", "password": "password123"})
+    assert res.status_code == 200

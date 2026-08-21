95f220e9f5a97daa82365ddbecb224d7567f73f3
amos <amoschikodiri1@gmail.com>
2026-08-21 19:16:12 +0100

    refactor: Move fixtures to conftest.py and fix JWT validation

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,17 +1,17 @@
 from app import schemas
+from jose import jwt
 from .database import client, session
 import pytest
+from app.config import settings
 
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email":  "test@example.com",
-                "password": "password123"}
-    res = client.post("/users/", json=user_data)
+
+
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "test@example.com", "password": "password123"})
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "test@example.com"
     assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
     
 #def test_root(client):
 #    res = client.get("/")
@@ -19,12 +19,13 @@ def test_user(client):
 #    assert res.json().get("message") == "Bind mount is now working"
 #    assert res.status_code == 200
 
-def test_create_user(client):
-    res = client.post("/users/", json={"email": "test@example.com", "password": "password123"})
-    new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "test@example.com"
-    assert res.status_code == 201
+
 
 def test_login_user(client, test_user):
     res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == str(test_user['id'])
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200

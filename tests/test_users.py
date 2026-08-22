3a2bbe4bc007ee69502391534953853da4f85b15
amos <amoschikodiri1@gmail.com>
2026-08-22 18:28:25 +0100

    test: Add parametrized tests for incorrect login attempts

--- tests/test_users.py
+++ tests/test_users.py
@@ -29,3 +29,17 @@ def test_login_user(client, test_user):
     assert id == str(test_user['id'])
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('test@example.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('test@example.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    
+    assert res.status_code == status_code
+    #assert res.json().get("detail") == "Invalid Credentials"]
